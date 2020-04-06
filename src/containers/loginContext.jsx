fc4da5cd1e2a20c1ad06847950107905a9296bfd
Yao Lei <yao_lei_java@hotmail.com>
2020-04-06 21:19:53 +0800

    add new index page

--- src/containers/loginContext.jsx
+++ src/containers/loginContext.jsx
@@ -2,6 +2,11 @@ import React, {useState, useContext} from 'react';
 import { BrowserRouter as Router, Link, Redirect, Route, Switch} from 'react-router-dom';
 import { Input, Button } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
+// var __html = require('../util/tmp/index');
+import data from '../util/tmp/index.html'
+import data1 from '../util/tmp/index'
+import Header from '../util/tmp/header'
+// var template = {__html: __html};
 
 class LoginContext extends React.Component {
     
@@ -20,7 +25,17 @@ class LoginContext extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <Button onClick={this.handleClickSubmitBut}>99</Button>
+                {/* <Button onClick={this.handleClickSubmitBut}>99</Button> */}
+                {/* <img src={img1} className="img-responsive" alt="Thin Laptop"/> */}
+                {/* <iframe
+                    ref= "iframe"
+                    title="resg"
+                    srcDoc={data1}
+                    style={{ width: '100%', border: '0px', height: '1100px' }}
+                    sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+                    scrolling="auto"
+                /> */}
+                <Header />
             </React.Fragment>            
         )
     }
